4fd918d2bce3adcbe5d2aeaac03877a38bdb7cdc
Yuan zhou <zhouy40630@gmail.com>
2018-05-07 12:05:55 -0400

    spinning earth

--- MeteorShower/ViewController.swift
+++ MeteorShower/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    let earthRadiusKm:CGFloat = 6371
+    let moonRadiusKm:CGFloat = 1731.5
+    let earthMoonDistance:CGFloat = 384400
+    let scale:CGFloat = 1/50000
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin,ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.autoenablesDefaultLighting = true
+        self.sceneView.session.run(self.configuration)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func pressedAddEarth(_ sender: Any) {
+        let earth = SCNNode()
+        earth.geometry = SCNSphere(radius: earthRadiusKm*scale)
+        earth.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "earth_daymap")
+        earth.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "earth_specular_map")
+        earth.geometry?.firstMaterial?.emission.contents = #imageLiteral(resourceName: "earth_clouds")
+        earth.geometry?.firstMaterial?.normal.contents = #imageLiteral(resourceName: "earth_elevation_normal_map")
+        
+        
+        earth.position = SCNVector3(0,0,-0.5)
+        
+        sceneView.scene.rootNode.addChildNode(earth)
+        let earthRotationAction = SCNAction.rotateBy(x: 0, y:CGFloat(360.degressToRadians), z: 0, duration: 8.0)
+        let earthRotateForever = SCNAction.repeatForever(earthRotationAction)
+        earth.runAction(earthRotateForever)
     }
+    
 
-
+    @IBAction func pressedAddMoon(_ sender: Any) {
+    }
 }
 
+extension Int{
+    var degressToRadians:Double{
+        return Double(self) * .pi/180
+    }
+}
