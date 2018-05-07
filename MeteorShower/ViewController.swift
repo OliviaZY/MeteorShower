a4f5d424067be33f6c6600e88e15d8f75ace6a46
Yuan zhou <zhouy40630@gmail.com>
2018-05-07 11:29:46 -0400

    Initial Commit

---
+++ MeteorShower/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeteorShower
+//
+//  Created by Yuan zhou on 5/7/18.
+//  Copyright © 2018 Yuan zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
